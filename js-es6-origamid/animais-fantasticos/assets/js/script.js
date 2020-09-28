731bb1633549a98b8455141213589db50e945daf
Gustavo Schneider <gustavo.schneider007@gmail.com>
2020-09-27 22:43:16 -0300

    add dropdown menu

--- js-es6-origamid/animais-fantasticos/assets/js/script.js
+++ js-es6-origamid/animais-fantasticos/assets/js/script.js
@@ -2,10 +2,14 @@ import initSmoothScroll from './modules/smooth-scroll.js';
 import initScrollAnimation from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-navigation.js';
-import initModal from './modules/modal.js'
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initSmoothScroll();
 initScrollAnimation();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
+initDropdownMenu();
